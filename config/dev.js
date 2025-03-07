c6ce29b103432a1f754348ef9f211ba87099ba24
abhi-joshi66 <103101091+abhi-joshi66@users.noreply.github.com>
2025-03-07 15:27:03 +0530

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
   googleClientID: '',
-  googleClientSecret: '',
-  mongoURI: '',
+  googleClientSecret: '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',
+  mongoURI: 'mongodb+srv://abhijitjoshi66:abhijitjoshi66@nodewithreactcluster.vqrzw.mongodb.net/?retryWrites=true&w=majority&appName=NodeWithReactCluster',
   cookieKey: 'jlkjfdklajfdajiejijijfdf',
 };
