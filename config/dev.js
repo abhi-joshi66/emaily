5e5aed6a48f5fdfab7239b8871298acd66477990
abhi-joshi66 <103101091+abhi-joshi66@users.noreply.github.com>
2025-03-07 16:22:55 +0530

    Update dev.js
    removed googleClient secret

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
   googleClientID: '',
-  googleClientSecret: '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',
+  googleClientSecret: '',
   mongoURI: 'mongodb+srv://abhijitjoshi66:abhijitjoshi66@nodewithreactcluster.vqrzw.mongodb.net/?retryWrites=true&w=majority&appName=NodeWithReactCluster',
   cookieKey: 'jlkjfdklajfdajiejijijfdf',
 };
